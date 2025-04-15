d53384aa4305a31111cfaa34e8b9b08ace4af265
YasinBB00 <yasin.bachi.bensaad@gmail.com>
2025-04-15 18:23:41 +0200

    Added unit test for test_forrmat_welcome_message

--- tests/unit/unit_tests.py
+++ tests/unit/unit_tests.py
@@ -3,6 +3,10 @@ import pytest
 from rendering import format_hello_greeting, format_visit_details, format_welcome_message, format_visit_history
 
 def test_format_welcome_message():
- sample_visit = {"id" : 123}
- expected_result = "Welcome", f"Welcome, you are visitor number {sample_visit['id']}"
- assert format_welcome_message(sample_visit) == expected_result
+ 
+    sample_visit = {"id" : 123}
+    expected_text = f"Welcome, you are visitor number {sample_visit['id']}"
+
+    actual_html = format_welcome_message(sample_visit)
+
+    assert expected_text in actual_html
