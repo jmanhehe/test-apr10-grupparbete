b910afec50060ca24566fcdd3d24b89996b14a1d
yaysin <ykarakartal>
2025-04-16 12:20:45 +0200

    Tre integrationstester som inte behöver ansluta till databasen har lagts till

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -10,4 +10,22 @@ os.makedirs("db", exist_ok=True)
 
 @pytest.fixture
 def client():
+    app.config['TESTING'] = True
+    return app.test_client()
 
+
+def test_hello_default(client):
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert b"Hello, mysterious visitor" in response.data
+
+def test_hello_with_name(client):
+    response = client.get('/hello?name=Yasin')
+    assert response.status_code == 200
+    assert b"Hello, Yasin" in response.data
+
+def test_hello_form(client):
+    response = client.get('/hello-form')
+    assert response.status_code == 200
+    assert b"Say Hello" in response.data
+    assert b"Your name:" in response.data
