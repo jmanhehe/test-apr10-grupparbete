1f9a5c9de1f23e5e0d49e05c937594b61886e606
j <jforsberg.se@gmail.com>
2025-04-18 16:40:25 +0200

    Revert "Began writing isolated test for adding a visit"
    This reverts commit eabebb84ebfa134a3bb31863cb7729c08aef3f8c.

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -1,7 +1,7 @@
 import pytest
 import psycopg2
 
-from db import get_db_connection, init_db, add_visit, get_visit_by_id, get_all_visits
+from db import get_db_connection, init_db, add_visit, get_visit_by_id
 from datetime import datetime, timezone
 
 @pytest.fixture(scope="module")
@@ -50,10 +50,6 @@ def test_init_db(db_setup):
         cur.close()
         conn.close()
 
-# def test_add_visit_isolated(db_setup):
-#     # test add visit separate first
-#     visit_data = add_visit(TEST_IP, USER_AGENT)
-
 def test_add_and_get_visit(clean_db):
     ip = "127.0.0.1"
     user_agent = "TestUserAgent"
@@ -73,7 +69,7 @@ def test_add_and_get_visit(clean_db):
     assert specific_visit["id"] == visit_data["id"], "Visit ID doesnt match against specific ID"
     assert specific_visit["ip"] == ip, "IP doesnt match"
     assert specific_visit["user_agent"] == user_agent, "Specific user agent doesnt match"
-
+    
     # Same as above, timestamp needs to be checked at
     # assert specific_visit["timestamp"] == visit_data["timestamp"], "Timestamp does not match"
 
