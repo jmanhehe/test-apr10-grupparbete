898ae8bdecc363674746121109d9c7e4a1711614
j <jforsberg.se@gmail.com>
2025-04-18 16:40:10 +0200

    Revert "Added testfile to check that env variables is correct"
    This reverts commit 0a30b27a783b3a106aac6001e1fda3f0e9fb3438.

--- tests/integration/test_env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def test_env():
-    print("DB_HOST:", os.getenv("DB_HOST"))
-    print("DB_PORT:", os.getenv("DB_PORT"))
-    print("DB_NAME:", os.getenv("DB_NAME"))
-    print("DB_USER:", os.getenv("DB_USER"))
-    print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
