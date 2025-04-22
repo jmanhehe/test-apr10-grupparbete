83a21329915454b8cc7e69276e34ecaf9b82bdab
j <jforsberg.se@gmail.com>
2025-04-22 08:43:07 +0200

    somehow get_all_visits got lost in space so i added it again

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -1,7 +1,7 @@
 import pytest
 import psycopg2
 
-from db import get_db_connection, init_db, add_visit, get_visit_by_id
+from db import get_db_connection, init_db, add_visit, get_visit_by_id, get_all_visits
 from datetime import datetime, timezone
 
 TEST_IP = "127.0.0.1"
